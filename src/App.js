9b788e9313d1d36a3a0ec41705579076eab68567
Poul <poulnichols@outlook.com>
2021-04-29 14:17:00 -0700

    Current changes to App.css/js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
+import React from "react";
+import NavBar from "./Navbar/NavBarComponent";
+import "./App.css";
+import CarouselSlider from "./components/CarouselSlider";
+import Footer from "./FooterComponent";
+import { Route, Switch, NavLink } from "react-router-dom";
 
-import React from 'react';
-import NavBar from './Navbar/NavBarComponent';
+class MainPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <div id="main-header-scoreboard">
+          <CarouselSlider />
+        </div>
+        <div id="main-page-nav">
+          <NavBar />
+        </div>
 
-
-
-
-function App() {
-  return (
-    
-    <div >
-      <NavBar />
-    </div>
-  );
+        <div id="main-page-wrapper"></div>
+        <div id="main-footer">
+          <Footer />
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default MainPage;
