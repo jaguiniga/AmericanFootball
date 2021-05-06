15a4735b545416a2d3c70747cc5f024b4cc85bff
Poul <poulnichols@outlook.com>
2021-05-06 07:37:57 -0700

    checkpoint

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Component } from "react";
 import NavBar from "./Navbar/NavBarComponent";
 import "./App.css";
-
+import { Container, Row, Col } from "bootstrap-4-react";
 import CarouselSlider from "./components/CarouselSlider";
 import WelcomeLogo from "./components/WelcomeLogo";
 import LoginComponent from "./components/LoginComponent";
 import Footer from "./FooterComponent";
 import Headlines from "./components/HeadlinesComponent";
 import "bootstrap/dist/css/bootstrap.css";
+import $ from "jquery";
 
 function MainPage() {
   return (
@@ -20,12 +21,17 @@ function MainPage() {
         <NavBar />
       </div>
       <div id="main-page-wrapper">
+        {/* Left Side */}
         <div className="column_left">
           <Headlines />
         </div>
+
+        {/* Center */}
         <div className="column_center">
           <WelcomeLogo />
         </div>
+
+        {/* Right Side */}
         <div className="column_right">
           <Headlines />
           <Headlines />
