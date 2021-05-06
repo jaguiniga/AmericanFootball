61a3c4c49b32660f2b6c97be2b92724c01f7d33a
Poul <poulnichols@outlook.com>
2021-05-06 08:01:34 -0700

    checkpoint

--- src/components/WelcomeLogo.js
+++ src/components/WelcomeLogo.js
@@ -5,8 +5,11 @@ import "../css/WelcomeLogo.css";
 function WelcomeLogo() {
   return (
     <div className="autoFit">
-      <h1>Welcome To American Football</h1>
-      <img src={Logo} className="autoFit" alt="Football Player" />
+      {/* <h1>Welcome To American Football</h1> */}
+      <div>
+        <img src={Logo} className="image-autoFit" alt="Football Player" />
+        Put some test here
+      </div>
     </div>
   );
 }
