ea450a1f98da138e6aa496895da43bac405142e0
Poul <poulnichols@outlook.com>
2021-05-11 13:00:21 -0700

    checkpoint

---
+++ src/components/ScoreCard.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.css";
+
+function ScoreCard() {
+  return (
+    <div className="container">
+      <div className="card rounded-top m-5">
+        <div className="card-body"></div>
+        <div className="card-footer"></div>
+      </div>
+    </div>
+  );
+}
+
+export default ScoreCard;
