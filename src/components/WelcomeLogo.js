d948ff67612d1256cea9c1978a80f1df262a6196
Poul <poulnichols@outlook.com>
2021-05-26 11:12:50 -0700

    Update website colors

--- src/components/WelcomeLogo.js
+++ src/components/WelcomeLogo.js
@@ -3,7 +3,12 @@ import "../css/WelcomeLogo.css";
 
 function WelcomeLogo() {
   return (
-    <div className="autoFit">
+    <div
+      className="autoFit"
+      style={{
+        boxShadow:
+          "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+      }}>
       {/* <h1>Welcome To American Football</h1> */}
       <div>
         <img src={Logo} className="image-autoFit" alt="Football Player" />
