2e3fb70d82250d724197123ec1c98d9d5b6b0b7b
Poul <poulnichols@outlook.com>
2021-05-26 12:42:07 -0700

    checkpoint

--- src/components/HeadlinesComponent.js
+++ src/components/HeadlinesComponent.js
@@ -6,20 +6,25 @@ import Logo from "../images/NFL_Draft_reglogo.png";
 import ProfilePage from "../Profile/ProfilePage";
 const Headlines = () => {
   return (
-  <section>
-  <div className="card border-primary mb-3" style = {{maxWidth: "18rem"}}>
-  <div className="card-header"><strong>Profile</strong></div>
-  <ProfilePage/>
-
-</div>
-<div className="card border-secondary mb-3" style = {{maxWidth: "18rem"}}>
-<div className="card-header"><strong>Top Headlines</strong></div>
-     <div className="card-body text-primary">
-     <img src={Logo} className="image-autoFit" alt="NFL Logo" />
-     <ul className="links" >
-         <li><a href="https://www.sbnation.com/nfl-draft">
-                 2021 NFL Draft Starts
-         </a></li>
+    <section>
+      <div className="card border-primary mb-3" style={{ maxWidth: "18rem" }}>
+        <div className="card-header">
+          <strong>Profile</strong>
+        </div>
+        <ProfilePage />
+      </div>
+      <div className="card border-secondary mb-3" style={{ maxWidth: "18rem" }}>
+        <div className="card-header">
+          <strong>Top Headlines</strong>
+        </div>
+        <div className="card-body text-primary">
+          <img src={Logo} className="image-autoFit" alt="NFL Logo" />
+          <ul className="links">
+            <li>
+              <a href="https://www.sbnation.com/nfl-draft">
+                2021 NFL Draft Starts
+              </a>
+            </li>
 
             <li>
               <a href="https://www.si.com/nfl/dolphins/news/complete-2021-nfl-draft-team-by-team-rundown">
@@ -27,27 +32,36 @@ const Headlines = () => {
               </a>
             </li>
 
-         <li><a href="https://www.pff.com/news/draft-film-study-pros-and-cons-2021-nfl-draft">
-                  Pros and Cons For The 2021 NFL Draft
-         </a></li>
-  </ul>
-  </div>
-</div>
-<div className="card border-success mb-3" style = {{maxWidth: "18rem"}}>
-<div className="card-header"><strong>This Week's Top Video</strong></div>
-  <div className="card-body text-secondary">
-  {/* <video controls width="250"> */}
-  
-      <iframe width="230" height="315" src="https://www.youtube.com/embed/BP7kUVZW5Gk" title="YouTube video player" frameborder="0" 
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; 
-              picture-in-picture" allowfullscreen>
-      </iframe>
-       {/* <ReactPlayer 
+            <li>
+              <a href="https://www.pff.com/news/draft-film-study-pros-and-cons-2021-nfl-draft">
+                Pros and Cons For The 2021 NFL Draft
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="card border-success mb-3" style={{ maxWidth: "18rem" }}>
+        <div className="card-header">
+          <strong>This Week's Top Video</strong>
+        </div>
+        <div className="card-body text-secondary">
+          {/* <video controls width="250"> */}
+
+          <iframe
+            width="230"
+            height="315"
+            src="https://www.youtube.com/embed/BP7kUVZW5Gk"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; 
+              picture-in-picture"
+            allowfullscreen></iframe>
+          {/* <ReactPlayer 
         url="https://www.youtube.com/watch?v=ug50zmP9I7s"
       />  */}
-       {/* </video>  */}
-  </div>
-</div>
+          {/* </video>  */}
+        </div>
+      </div>
 
       <div
         className="card border-danger mb-3"
@@ -88,7 +102,7 @@ const Headlines = () => {
           boxShadow:
             "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
         }}>
-        <div className="card-header">Header3</div>
+        {/* <div className="card-header">Header3</div>
         <div className="card-body text-info">
           <h5 className="card-title">Info card title</h5>
           <p className="card-text">
@@ -103,8 +117,8 @@ const Headlines = () => {
           maxWidth: "18rem",
           boxShadow:
             "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
-        }}>
-        {/*  <div className="card-header">Header</div>
+        }}> */
+        /*  <div className="card-header">Header</div>
   <div className="card-body text-dark">
     <h5 className="card-title">Dark card title</h5>
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
