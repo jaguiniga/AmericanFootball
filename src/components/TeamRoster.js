ad25f000a34ea27a618b464d4614174705deae84
SherrodWare <kungskii88@gmail.com>
2021-06-11 22:53:12 -0700

    Fixed Team Roster Card and Main Matchup Card

--- src/components/TeamRoster.js
+++ src/components/TeamRoster.js
@@ -18,10 +18,10 @@ const TeamRoster = () => {
         </div>
         <div className="card-body text-secondary">
         <div className="card-body text-danger">
-        <h5 className="card-title">View Your Team</h5>
+        <div className="card-title">View Your Team</div>
           <ul className="links">
-            <li style = {{color:"black"}}>
-            {/* <h5>  <a href="https://www.sbnation.com/nfl-draft">Dallas Cowboys</a></h5> */}
+            <li>
+            
             {Roster.map((data, key) => { 
               return(
                 <div key = {key}>
@@ -36,22 +36,7 @@ const TeamRoster = () => {
             </li>
           </ul>
 
-          <ul className="links">
-            <li className="bullets">
-              {" "}
-            <h5>  <a href="https://www.si.com/nfl/dolphins/news/complete-2021-nfl-draft-team-by-team-rundown">
-                Green Bay Packers
-              </a></h5>
-            </li>
-          </ul>
-
-          <ul className="links">
-            <li className="bullets">
-            <h5> <a href="https://www.pff.com/news/draft-film-study-pros-and-cons-2021-nfl-draft">
-                Jacksonville Jaguars
-              </a></h5>
-            </li>
-          </ul>
+        
         </div>
       </div>
      </div>
